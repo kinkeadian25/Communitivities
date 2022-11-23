d9081631cf29b86f2224d2df6d500c22887a539d
kinkeadian25 <kinkeadian@gmail.com>
2022-11-22 23:46:41 -0800

    beginning api error handling

--- API/Startup.cs
+++ API/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Core;
 using API.Extensions;
 using Application.Activities;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -32,7 +33,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(config => 
+            {
+                config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
             services.AddApplicationServices(_config);
         }
 
